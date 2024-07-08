e2fd4cd8dd81908a97a9f4bee6e82ee43e1ee356
Zakaria Lachkar <97468839+LACHKAR99ZAKARIA@users.noreply.github.com>
2024-07-08 10:34:31 +0100

    Initial Commit

---
+++ Swiftui test refresher/Swiftui_test_refresherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_test_refresherApp.swift
+//  Swiftui test refresher
+//
+//  Created by Zakaria Lachkar on 8/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_test_refresherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
